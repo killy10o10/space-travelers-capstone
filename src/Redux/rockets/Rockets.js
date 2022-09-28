00a0fe33884150d8ab3229750d98c5ab16510f59
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2022-09-28 01:36:27 +0000

    Implement feature for reserving rockets
    Display reserved rockets in profile page

--- src/Redux/rockets/Rockets.js
+++ src/Redux/rockets/Rockets.js
@@ -1,38 +1,40 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
+/* eslint-disable max-len */
 
-const SHOW_ROCKETS = 'rockets/rockets/SHOW_ROCKETS';
+const RETRIEVE_ASYNC_ROCKETS = 'space-travlers/rockets/RETRIEVE_ROCKETS';
+const RETRIEVE_ROCKETS = 'space-travlers/rockets/RETRIEVE_ROCKETS/fulfilled';
+const BOOK_ROCKET = 'space-travlers/rockets/BOOK_ROCKET';
+const LEAVE_ROCKET = 'space-travlers/rockets/LEAVE_ROCKET';
 
-const url = 'https://api.spacexdata.com/v3/rockets/';
-const initialState = { rocketsArray: [] };
+const url = 'https://api.spacexdata.com/v3/rockets';
+const initialState = [];
 
 export const showRockets = createAsyncThunk(
-  SHOW_ROCKETS,
-  async (args, { dispatch }) => {
+  RETRIEVE_ASYNC_ROCKETS,
+  async () => {
     const response = await fetch(url);
     const data = await response.json();
-    const rockets = Object.keys(data).map((key) => {
-      const rocket = data[key];
-      return {
-        id: key,
-        ...rocket,
-      };
-    });
-    dispatch({
-      ...rockets,
-      type: SHOW_ROCKETS,
-      payload: rockets,
-    });
+    const rockets = data.map((item) => ({
+      id: item.id,
+      name: item.rocket_name,
+      description: item.description,
+      image: item.flickr_images,
+    }));
     return rockets;
   },
 );
 
+export const bookRocket = (payload) => ({ type: BOOK_ROCKET, payload });
+export const leaveRocket = (payload) => ({ type: LEAVE_ROCKET, payload });
+
 const rocketReducer = (state = initialState, action) => {
   switch (action.type) {
-    case SHOW_ROCKETS:
-      return {
-        ...state,
-        rocketsArray: action.payload,
-      };
+    case RETRIEVE_ROCKETS:
+      return action.payload;
+    case BOOK_ROCKET:
+      return state.map((rocket) => (rocket.id === action.payload ? { ...rocket, reserved: true } : rocket));
+    case LEAVE_ROCKET:
+      return state.map((rocket) => (rocket.id === action.payload ? { ...rocket, reserved: false } : rocket));
     default:
       return state;
   }
--- src/components/Rocket.jsx
+++ src/components/Rocket.jsx
@@ -1,16 +1,36 @@
+/* eslint-disable react/forbid-prop-types */
 import React from 'react';
 import './rocket.css';
+import { useDispatch } from 'react-redux';
+import { bookRocket, leaveRocket } from '../Redux/rockets/Rockets';
 
 export default function Rocket(props) {
+  const { rocket } = props;
+  const dispatch = useDispatch();
+  const reserveRocket = () => dispatch(bookRocket(rocket.id));
+  const cancelRocket = () => dispatch(leaveRocket(rocket.id));
+
   return (
     <section className="rockets-section">
       <div className="">
-        <img src={props.image} alt="rocket" />
+        <img src={rocket.image} alt={rocket.name} />
       </div>
       <div className="details">
-        <h1>{props.name}</h1>
-        <p>{props.description}</p>
-        <button className="button" type="button">Reserve Rocket</button>
+        <h1>{rocket.name}</h1>
+        <p>
+          {rocket.reserved && <small className="reserve">Reserved</small>}
+          {rocket.description}
+        </p>
+        {!rocket.reserved && (
+          <button className="reserve button" type="button" onClick={() => reserveRocket()}>
+            Reserve Rocket
+          </button>
+        )}
+        {rocket.reserved && (
+          <button className="cancel button" type="button" onClick={() => cancelRocket()}>
+            Cancel Reservation
+          </button>
+        )}
       </div>
     </section>
   );
