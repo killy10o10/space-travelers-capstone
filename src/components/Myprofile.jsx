2658107b98c288fa626465c528408f869bd18ddf
Hamid Ali <hammali99@gmail.com>
2022-09-28 19:10:39 +0500

    Add catch reserved function
    use filter to catch reserved items

--- src/components/Myprofile.jsx
+++ src/components/Myprofile.jsx
@@ -1,30 +1,41 @@
-import React from 'react';
-import './myprofile.css';
-import { useSelector } from 'react-redux';
-import Table from 'react-bootstrap/Table';
+import React from "react";
+import "./myprofile.css";
+import { useSelector } from "react-redux";
+import Table from "react-bootstrap/Table";
 
 export default function Myprofile() {
-  const reservedRockets = useSelector((state) => (
-    state.rocket.filter((item) => item.reserved)
-  ));
+  const reservedRockets = useSelector((state) => state.rocket.filter((item) => item.reserved));
+
+  const { missionArray } = useSelector((state) => state.mission);
+
+  const reservedMission = missionArray.filter((mission) => mission.reserved === !true);
+
   return (
     <div className="profile-content">
       <section className="reserved-rockets">
         <h1>My Rockets</h1>
         <Table bordered>
           <tbody>
-            {
-              reservedRockets.map((rocket) => (
-                <tr key={rocket.id}>
-                  <td>{rocket.name}</td>
-                </tr>
-              ))
-          }
+            {reservedRockets.map((rocket) => (
+              <tr key={rocket.id}>
+                <td>{rocket.name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </section>
+      <section>
+        <h1>My Missions</h1>
+        <Table bordered>
+          <tbody>
+            {reservedMission.map((mission) => (
+              <tr key={mission.id}>
+                <td>{mission.mission_name}</td>
+              </tr>
+            ))}
           </tbody>
         </Table>
       </section>
     </div>
-
-
   );
 }
