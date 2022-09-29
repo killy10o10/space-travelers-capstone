94f5407beae3400101a5d57f6b0cf971d71712a7
Hamid Ali <hammali99@gmail.com>
2022-09-29 15:42:18 +0500

    Added test for profile mission and rocket

---
+++ src/components/__test__/__mock__/myprofile.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function Myprofile() {
+  return (
+    <div className="profile-content">
+      <section className="reserved-rockets">
+        <h1>My Rockets</h1>
+        <table>
+          <tbody>
+            <tr>
+              <td>rocket</td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+      <section>
+        <h1>My Missions</h1>
+        <table>
+          <tbody>
+            <tr>
+              <td>mission</td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+    </div>
+  );
+}
